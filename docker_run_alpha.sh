2c195ce8a92d01288560af598560580fb7c2de29
Michael de Silva <michael@mwdesilva.com>
2014-11-01 11:10:29 +0530

    WIP generalise run script, so it could be used for other apps

--- docker_run_alpha.sh
+++ docker_run_alpha.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+ORG=inertialbox
 APP_NAME=inertialbox
+SECRET_KEY_BASE=foo
+MYSQL_ROOT_PASSWORD=0mDF30W43I
+MYSQL_PASSWORD=A307W7oP52j6Fxv
 
 docker ps -a | grep "mysql[^\-]" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-  docker run -d --name mysql --restart=on-failure:5 -v /tmp:/tmp -v /etc/mysql:/etc/mysql -v /var/lib/mysql:/var/lib/mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=0mDF30W43I" -e "MYSQL_USER=${APP_NAME}" -e "MYSQL_PASSWORD=A307W7oP52j6Fxv" -e "MYSQL_DATABASE=${APP_NAME}_production" mysql:5.7
+  docker run -d --name mysql --restart=on-failure:5 -v /tmp:/tmp -v /etc/mysql:/etc/mysql -v /var/lib/mysql:/var/lib/mysql -p 3306:3306 -e "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" -e "MYSQL_USER=${APP_NAME}" -e "MYSQL_PASSWORD=${MYSQL_PASSWORD}" -e "MYSQL_DATABASE=${APP_NAME}_production" mysql:5.7
 fi
 
-[ -n "$REBUILD" ] && docker build -t inertialbox/inertialbox-app ~/hack/inertialbox.com
+# Build app
+build_app()
+{
+  echo -e "\n===> Commencing app rebuild...\n"
+  docker build -t ${ORG}/${APP_NAME}-app ~/hack/inertialbox.com
+  echo -e "\n===> Completed app rebuild.\n"
+}
+
+docker images | grep "^${ORG}/${APP_NAME}-app" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  build_app
+elif [ -n "$REBUILD" ]; then
+  build_app
+fi
 
 docker ps -a | grep "[^-]app\b" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   docker stop app && docker rm app
 fi
-docker run -d --name app --restart=on-failure:5 -v /var/log/nginx/:/var/log/nginx/ -v /var/lib/mysql:/var/lib/mysql -v /tmp:/tmp -p 80:80 -e "SECRET_KEY_BASE=secretkey" --link mysql:webdb inertialbox/inertialbox-app
+docker run -d --name app --restart=on-failure:5 -v /var/log/nginx/:/var/log/nginx/ -v /var/lib/mysql:/var/lib/mysql -v /tmp:/tmp -p 80:80 -e "SECRET_KEY_BASE=${SECRET_KEY_BASE}" --link mysql:webdb ${ORG}/${APP_NAME}-app
 
 echo -e "\n===> Done.\n"
