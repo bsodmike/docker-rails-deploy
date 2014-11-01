768ef2569ef20aaec0c345ba5ab2ae5d3c123272
Michael de Silva <michael@mwdesilva.com>
2014-11-01 17:48:53 +0530

    WIP initially working failover setup :clap:

--- failover.sh
+++ failover.sh
@@ -1,6 +1,4 @@
-## TODO
-* app2 needs a unique socket for unicorn (Done)
-
+#!/bin/bash
 
 docker run -d --name app --restart=on-failure:5 -v /var/log/nginx/:/var/log/nginx/ -v /var/lib/mysql:/var/lib/mysql -v /tmp:/tmp -p 8080:80 -e "SECRET_KEY_BASE=foo" --link mysql:webdb inertialbox/inertialbox-app
 
@@ -10,3 +8,7 @@ docker build -t inertialbox/inertialbox-app-failover ~/hack/inertialbox.com_fail
 
 
 docker run -d --name app-failover -v /var/log/nginx-app-failover/:/var/log/nginx/ -v /tmp:/tmp -p 8081:80 -e "SECRET_KEY_BASE=foo2" --link mysql:webdb inertialbox/inertialbox-app-failover
+
+
+docker build -t inertialbox/nginx-load-balancer nginx-load-balancer
+docker run -d --name load-balancer -v /var/log/nginx-load-balancer/:/var/log/nginx/ -p 80:80 inertialbox/nginx-load-balancer
