16f8549938bae63f438f26a637a7f0a0895958b2
Michael de Silva <michael@mwdesilva.com>
2014-11-01 10:13:33 +0530

    Use relative paths, essential for making this a viable solution

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t inertialbox/trusty-base ~/hack_ops/packer-digitalocean-docker/docker/trusty_base
-docker build -t inertialbox/rails-nginx-unicorn ~/hack_ops/packer-digitalocean-docker/docker/rails-nginx-unicorn
+docker build -t inertialbox/trusty-base trusty_base
+docker build -t inertialbox/rails-nginx-unicorn rails-nginx-unicorn
