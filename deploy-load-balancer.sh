2ca2338bbc89e78fac0cc272159e6c96ea61c5e5
Michael de Silva <michael@mwdesilva.com>
2014-11-02 03:53:15 +0530

    Rebuilding load-balancer should restart it

--- deploy-load-balancer.sh
+++ deploy-load-balancer.sh
@@ -13,10 +13,17 @@ docker images | grep "^${ORG}/load-balancer-one" > /dev/null 2>&1
 [ $? -ne 0 ] && echo -e "\n===> Building image for load balancer ONE.\n" &&\
   docker build -t ${ORG}/load-balancer-one inertialbox/load-balancer-one
 
-[ -n "$REBUILD" ] && echo -e "\n===> Re-building image for load balancer ONE.\n" &&\
+if [ -n "$REBUILD" ];then
+  echo -e "\n===> Re-building image for load balancer ONE.\n"
   docker build -t ${ORG}/load-balancer-one inertialbox/load-balancer-one
 
-docker ps -a | grep "[^\-]load-balancer-one" > /dev/null 2>&1
+  docker ps -a | grep "[^\-\/]load-balancer-one" > /dev/null 2>&1
+  [ $? -eq 0 ] && echo -e "\n===> Stopping and removing load-balancer-one container.\n" &&\
+    docker stop load-balancer-one > /dev/null 2>&1 &&\
+    docker rm load-balancer-one > /dev/null 2>&1
+fi
+
+docker ps -a | grep "[^\-\/]load-balancer-one" > /dev/null 2>&1
 [ $? -ne 0 ] && echo -e "\n===> Running load balancer ONE.\n" && \
   docker run -d --name load-balancer-one \
   -v /var/log/load-balancer-one/:/var/log/nginx/ \
