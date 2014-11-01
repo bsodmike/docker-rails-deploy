2b7700ced0a3650a740587df8cd362e735f13d10
Michael de Silva <michael@mwdesilva.com>
2014-11-02 01:43:56 +0530

    Allow creating load balancers from base image

--- deploy-load-balancer.sh
+++ deploy-load-balancer.sh
@@ -3,9 +3,12 @@
 ORG=inertialbox
 
 docker images | grep "^${ORG}/nginx-load-balancer" > /dev/null 2>&1
-[ $? -ne 0 ] && echo -e "\n===> Building image for the load balancer.\n" && docker build -t ${ORG}/nginx-load-balancer nginx-load-balancer
+[ $? -ne 0 ] && echo -e "\n===> Building image for the base load balancer.\n" && docker build -t ${ORG}/nginx-load-balancer nginx-load-balancer
 
-docker ps -a | grep "[^\-]load-balancer" > /dev/null 2>&1
-[ $? -ne 0 ] && echo -e "\n===> Running the load balancer.\n" && docker run -d --name load-balancer -v /var/log/nginx-load-balancer/:/var/log/nginx/ -p 80:80 ${ORG}/nginx-load-balancer
+docker images | grep "^${ORG}/load-balancer-one" > /dev/null 2>&1
+[ $? -ne 0 ] && echo -e "\n===> Building image for load balancer ONE.\n" && docker build -t ${ORG}/load-balancer-one inertialbox/load-balancer-one
+
+docker ps -a | grep "[^\-]load-balancer-one" > /dev/null 2>&1
+[ $? -ne 0 ] && echo -e "\n===> Running load balancer ONE.\n" && docker run -d --name load-balancer-one -v /var/log/load-balancer-one/:/var/log/nginx/ -p 80:80 ${ORG}/load-balancer-one
 
 echo -e "\n===> Done.\n"
